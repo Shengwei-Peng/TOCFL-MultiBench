2f6a1021dc495ca37cddac1a5b1303d4952c01c9
Shengwei-Peng <m11207330@mail.ntust.edu.tw>
2024-11-03 07:51:47 +0800

    refactor: rename MultimodalModel class to MultimodalSystem and update implementation

--- src/utils.py
+++ src/utils.py
@@ -1,52 +1,53 @@
 """utils"""
+import os
+import gc
+
 import torch
 from PIL import Image
+from huggingface_hub import login
+from datasets import load_dataset
 from transformers import AutoProcessor, AutoModelForImageTextToText
 
 
-class MultimodalModel:
-    """MultimodalModel"""
-    def __init__(self, initial_model_name: str) -> None:
+class MultimodalSystem:
+    """MultimodalSystem"""
+    def __init__(self, model_name_or_path: str, dataset_name_or_path: str) -> None:
         """__init__"""
-        self.model_name = initial_model_name
-        self.processor = AutoProcessor.from_pretrained(
-            self.model_name, trust_remote_code=True
-        )
-        self.model = AutoModelForImageTextToText.from_pretrained(
-            self.model_name,
-            torch_dtype=torch.bfloat16,
-            trust_remote_code=True,
-            attn_implementation="flash_attention_2",
-            device_map="auto"
-        )
+        login(token=os.getenv("HUGGINGFACE_TOKEN"))
+        self.model = None
+        self.processor = None
+        self.model_name_or_path = None
+        self.dataset_name_or_path = None
+        self.load(model_name_or_path, dataset_name_or_path)
 
-    def update_model(self, model_name: str) -> None:
-        """update_model"""
-        if model_name != self.model_name:
-            self.model_name = model_name
+    def load(self, model_name_or_path: str, dataset_name_or_path: str) -> None:
+        """load"""
+        if model_name_or_path != self.model_name_or_path:
+            del self.model, self.processor
+            torch.cuda.empty_cache()
+            gc.collect()
+
+            self.model_name_or_path = model_name_or_path
             self.processor = AutoProcessor.from_pretrained(
-                self.model_name, trust_remote_code=True
+                self.model_name_or_path, trust_remote_code=True
             )
             self.model = AutoModelForImageTextToText.from_pretrained(
-                self.model_name,
+                self.model_name_or_path,
                 torch_dtype=torch.bfloat16,
                 trust_remote_code=True,
-                attn_implementation="flash_attention_2",
-                device_map="auto"
+                device_map="auto",
             )
 
-    def generate(self, image: Image, text: str, model_name: str) -> None:
-        """generate"""
-        self.update_model(model_name)
-        conversation = [
-            {
+        if dataset_name_or_path != self.dataset_name_or_path:
+            self.dataset_name_or_path = dataset_name_or_path
+            self.dataset = load_dataset(self.dataset_name_or_path)
 
-            "role": "user",
-            "content": [
-                {"type": "text", "text": text},
-                {"type": "image"},
-                ],
-            },
+        return str(self.dataset), str(self.model)
+
+    def generate(self, text: str, image: Image) -> None:
+        """generate"""
+        conversation = [
+            {"role": "user", "content": [{"type": "text", "text": text}, {"type": "image"}]}
         ]
         prompt = self.processor.apply_chat_template(
             conversation, add_generation_prompt=True
